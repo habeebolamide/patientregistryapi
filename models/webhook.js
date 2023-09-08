a06935f809651ee72ed35c4a687878c1fc6f3426
Habeeb Olamide <adenubiho@edutams.net>
2023-09-08 09:29:51 +0100

    fix

--- models/webhook.js
+++ models/webhook.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 
 const Webhook = new mongoose.Schema({
     webhook: { type: Array, required: true },
-});
+},{ timestamps: true });
 
 module.exports = mongoose.model('Webhook', Webhook);
