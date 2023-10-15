573f8b36eabafda5ceffcc1fc37b537d1524f9cb
Habeeb Olamide <adenubiho@edutams.net>
2023-10-15 20:19:58 +0100

    fix

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const patient = require('./routes/patient')
 const group = require('./routes/group')
 const disease = require('./routes/disease')
 app.use(express.json())
+app.set('view engine','ejs')
 app.use(cors());
+app.get('/', (req,res) => {
+    res.render('index')
+})
 app.use('/uploads', express.static('uploads'))
 app.use('/api/v1/patient', patient);
 app.use('/api/v1/group', group);
